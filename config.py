eb1bcf11523647c1b57068aba4d3f6a5bb4e9e64
Wilna-GrowUp <wilnalima@gmail.com>
2026-03-19 20:00:22 -0300

    Update config.py
    Atualizando config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "chave-local-simples")
-
-    # Render/PostgreSQL normalmente envia DATABASE_URL como variável de ambiente
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
-    # Evita aviso desnecessário do SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
