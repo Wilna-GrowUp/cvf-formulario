d60013ce22341391c85b69589b37cf7dbe3e9112
Wilna-GrowUp <wilnalima@gmail.com>
2026-03-20 10:33:22 -0300

    Update config.py
    config.py

--- config.py
+++ config.py
@@ -3,5 +3,18 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "chave-local-simples")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        if database_url.startswith("postgres://"):
+            database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+        SQLALCHEMY_DATABASE_URI = database_url
+    else:
+        SQLALCHEMY_DATABASE_URI = "sqlite:///cvf_local.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Senha simples do administrador
+    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin123")
