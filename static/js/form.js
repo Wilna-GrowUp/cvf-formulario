da0bb7f9b0ad929a518a10a93514168c373842ba
Wilna-GrowUp <wilnalima@gmail.com>
2026-03-20 13:56:44 -0300

    Resolve conflitos de merge no projeto

--- static/js/form.js
+++ static/js/form.js
@@ -149,8 +149,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     validarFormulario();
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> 4c591baf6ce64bd7edfeb8feb4bbcbfea24a9484
